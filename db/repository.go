7b37009d400abbd76b390c610273952edd596843
DOO-DEV <nimo.vimo.yank@gmail.com>
2023-09-03 18:40:53 +0330

    feat: add repository method implement with inversion of control pattern

---
+++ db/repository.go
@@ -0,0 +1,28 @@
+package db
+
+import (
+	"context"
+	"meower/schema"
+)
+
+type Repository interface {
+	Close()
+	InsertMeow(ctx context.Context, meow schema.Meow) error
+	ListMeows(ctx context.Context, skip, take uint64) ([]schema.Meow, error)
+}
+
+var implement Repository
+
+func SetRepository(repo Repository) {
+	implement = repo
+}
+
+func Close() {
+	implement.Close()
+}
+func InsertMeow(ctx context.Context, meow schema.Meow) error {
+	return implement.InsertMeow(ctx, meow)
+}
+func ListMeows(ctx context.Context, skip, take uint64) ([]schema.Meow, error) {
+	return implement.ListMeows(ctx, skip, take)
+}
